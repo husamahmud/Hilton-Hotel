1972e9dc80ebd14bc6335f6d76aadd111fe89cee
Hüsam <husamahmud@gmail.com>
2024-02-15 02:40:19 +0300

    feat[backend]: Create room reservation model

--- models/prisma/migrations/20240211122855_edit_user_is_deleted/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isDelted` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "isDelted",
-ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT false;
