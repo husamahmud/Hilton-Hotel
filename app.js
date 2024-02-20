0c0f2651c35a88c6d8db2f564a3014e809e40a3f
Kholoud Fattem <kfattem@gmail.com>
2024-02-20 05:46:17 +0200

    feat[backend]: Implement verifyToken and add it to the routes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import { config } from 'dotenv';
 
 config();
 
+import { verifyToken } from './utilities/token.js';
+
 const app = express();
 
 app.use(json());
@@ -12,7 +14,7 @@ app.use(urlencoded({ extended: false }));
  * admin route
  **/
 import adminRouter from './routes/admin.route.js';
-app.use('/api/v1/admin', adminRouter);
+app.use('/api/v1/admin', verifyToken, adminRouter);
 
 /**
  * auth route
@@ -24,79 +26,81 @@ app.use('/api/v1/auth', authRouter);
  * user route
  **/
 import userRoute from './routes/user.route.js';
-app.use('/api/v1/user', userRoute);
+app.use('/api/v1/user', verifyToken, userRoute);
 
 /**
  * room route
  **/
 import roomRoute from './routes/room.route.js';
-app.use('/api/v1/room', roomRoute);
+app.use('/api/v1/room', verifyToken, roomRoute);
 
 /**
  * room reservation route
  **/
 import roomReservationRoute from './routes/roomResevation.route.js';
-app.use('/api/v1/roomreservation', roomReservationRoute);
+app.use('/api/v1/roomreservation', verifyToken, roomReservationRoute);
 
 /**
  * menu route
  **/
 import menuRoute from './routes/menu.route.js';
-app.use('/api/v1/menu', menuRoute);
+app.use('/api/v1/menu', verifyToken, menuRoute);
 
 /**
  * resturant route
  **/
 import restaurantRoute from './routes/restaurant.route.js';
-app.use('/api/v1/restaurant', restaurantRoute);
+app.use('/api/v1/restaurant', verifyToken, restaurantRoute);
 
 /**
  * news route
  **/
 import newsRoute from './routes/news.route.js';
-app.use('/api/v1/news', newsRoute);
+app.use('/api/v1/news', verifyToken, newsRoute);
 
 /**
  * reply route
  **/
 import replyRoute from './routes/reply.route.js';
-app.use('/api/v1/reply', replyRoute);
+app.use('/api/v1/reply', verifyToken, replyRoute);
 
 /**
  * clubHouse route
  **/
 import clubHouseRoute from './routes/clubHouse.route.js';
-app.use('/api/v1/clubHouse', clubHouseRoute);
+app.use('/api/v1/clubHouse', verifyToken, clubHouseRoute);
 
 /**
  * contactUs route
  **/
 import contactUsRoute from './routes/contactUs.route.js';
-app.use('/api/v1/contact', contactUsRoute);
+app.use('/api/v1/contact', verifyToken, contactUsRoute);
 
 /**
  * app settings route
  **/
 import appSettingsRoute from './routes/appSettings.route.js';
-app.use('/api/v1/settings', appSettingsRoute);
+app.use('/api/v1/settings', verifyToken, appSettingsRoute);
 
 /**
  * review route
  **/
 import reviewRoute from './routes/review.route.js';
-app.use('/api/v1/review', reviewRoute);
+app.use('/api/v1/review', verifyToken, reviewRoute);
 
 /**
  * extra services route
  **/
 import extraServicesRoute from './routes/extraServices.route.js';
-app.use('/api/v1/extraservices', extraServicesRoute);
+app.use('/api/v1/extraservices', verifyToken, extraServicesRoute);
 
 /**
  * faqs route
  **/
 import faqsRoute from './routes/faqs.route.js';
-app.use('/api/v1/faqs', faqsRoute);
+app.use('/api/v1/faqs', verifyToken, faqsRoute);
+
+
 
 app.use((req, res) => {
   res.status(404).json({ message: '404: Not Found' });
--- utilities/token.js
+++ utilities/token.js
@@ -6,4 +6,25 @@ export const createToken = (dto, duration) => {
 	}, process.env.TOKEN_SECRET, {expiresIn: duration})
 }
 
-// TODO verify token
+export const verifyToken = (req, res, next) => {
+	const authHeader = req.headers['authorization']
+	const token = authHeader?.split(' ')[1]
+
+	if (!token) return res.status(401).send('Access Denied')
+
+	try {
+	const user = jwt.verify(token, process.env.TOKEN_SECRET)
+
+	req.user = user
+	next()
+	}
+	catch (err) {
+		res.status(400).send('Invalid Token')
+
+		// TODO : Refresh Token Assurances
+	}
+}
+
+
+// TODO Authorization
+
