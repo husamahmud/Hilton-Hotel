ae41da7f9ee37abadddc93a4877eb0ca453efb9b
Hüsam <husamahmud@gmail.com>
2024-03-03 18:27:24 +0300

    fix[backend]: Update login `Dto`

--- hilton-server/models/dto/login.dto.js
+++ hilton-server/models/dto/login.dto.js
@@ -3,6 +3,5 @@ export class LoginDto {
     this.email = bodyReq.email;
     this.username = bodyReq.username;
     this.password = bodyReq.password;
-    this.confirmPassword = bodyReq.confirmPassword;
   }
 }
