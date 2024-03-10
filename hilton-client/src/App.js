9cf8936d3b2acb3bf2684cd1c8be5f76193b0501
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-03-10 02:06:23 +0200

    Update App.js

--- hilton-client/src/App.js
+++ hilton-client/src/App.js
@@ -1,21 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import AuthRoutes from './routes/AuthRoutes.js';
-import {Home} from './components/Home/Home';
-
-function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/auth/*"
-				       element={<AuthRoutes />} />
-				<Route path="/"
-				       element={<Home />} />
-			</Routes>
-		</Router>
-	);
-=======
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AuthRoutes from "./routes/AuthRoutes.js";
@@ -38,7 +20,6 @@ function App() {
       </Routes>
     </Router>
   );
->>>>>>> Stashed changes
 }
 
 export default App;
